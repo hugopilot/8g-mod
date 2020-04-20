aae35f0abd34c094281ad295260bb545427a2fdf
Hugo Woesthuis <hugow2011@outlook.com>
2020-04-20 20:35:17 +0200

    added basic command templates

--- src/bot.py
+++ src/bot.py
@@ -1,2 +1,29 @@
 import discord
 import config
+from discord.ext import commands
+from discord.ext.commands import has_permissions, MissingPermissions
+import typing
+
+
+# Delete default help command
+bot = commands.Bot(command_prefix=config.prefix)
+bot.remove_command('help')
+
+# This decorator adds the command
+@bot.command()
+# The function name is the name of the command, unless specified.  
+async def ban(ctx, musr: typing.Union[discord.User, str] = None, *, reason: str = None):
+    pass
+
+@bot.command()
+async def kick(ctx, musr: typing.Union[discord.User, str] = None, *, reason: str = None):
+    pass
+
+@bot.command()
+async def mute(ctx, musr: typing.Union[discord.User, str] = None, *, reason: str = None):
+    pass
+
+@bot.command()
+async def whois(ctx, musr: typing.Union[discord.User, str] = None):
+    pass
+
