3bd00e5eda4607736b5695eb64017f264d760114
Hugo Woesthuis <hugow2011@outlook.com>
2020-05-23 15:46:37 +0200

    Add example config.py, fix perms

---
+++ src/config.py
@@ -0,0 +1,13 @@
+# General info
+token = "insert token here"
+version = "0.1.0"
+prefix = '??'
+
+# File locations
+databaseloc = '../data/main.db'
+logloc = '../data/logs/main.log'
+
+# Server bindings
+# All ID's should be int! 
+guild = 'insert id here'
+logch = 'insert id here'
